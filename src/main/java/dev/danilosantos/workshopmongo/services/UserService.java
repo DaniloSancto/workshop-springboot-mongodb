8a2b4c7c7efee0841dd9730f429d5350f12b1898
Danilo Santos <danilo.santos0404@hotmail.com>
2023-03-28 14:26:51 -0300

    Obtendo um usuário por id

--- src/main/java/dev/danilosantos/workshopmongo/services/UserService.java
+++ src/main/java/dev/danilosantos/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package dev.danilosantos.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dev.danilosantos.workshopmongo.domain.User;
 import dev.danilosantos.workshopmongo.repository.UserRepository;
+import dev.danilosantos.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
---
+++ src/main/java/dev/danilosantos/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package dev.danilosantos.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
